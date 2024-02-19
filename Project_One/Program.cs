94f103c7ad4c18fa2393ada47e18801006fb9237
Sapphire Godard <cng8060@g.rit.edu>
2024-02-19 17:58:41 -0500

    - Added working command line argument parsing and some basic logic handling for command line arguments

--- Project_One/Program.cs
+++ Project_One/Program.cs
@@ -1,4 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CommandLine;
+using CommandLine.Text;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Testing changes rq");
+namespace Project_One
+{
+    public class ProgramOptions
+    {
+        [Option(shortName: 's', Group = "threadMode", HelpText = "Run in single threaded mode")]
+        public bool SingleThread { get; set; }
+        
+        [Option(shortName: 'd', Group = "threadMode", HelpText = "Run in parallel mode (uses all available processors)")]
+        public bool Parallel { get; set; }
+        
+        [Option(shortName: 'b', Group = "threadMode", HelpText = "Run in both parallel and single threaded mode. Runs parallel followed by sequential mode")]
+        public bool Both { get; set; }
+        
+        [Value(index: 0, MetaName = "Path", Required = true, HelpText = "Starting directory to parse through")]
+        public string? Path { get; set; }
+
+        [Usage(ApplicationAlias = "du")]
+        public static IEnumerable<Example> Examples
+        {
+            get 
+            {
+                return new List<Example>()
+                {
+                    new Example("Summarize disk usage of the set of FILES, recursively for directories",
+                        new ProgramOptions { Both = true, Path = "C:/Windows"})
+                };
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<ProgramOptions>(args).WithParsed(options =>
+            {
+                if (!Directory.Exists(options.Path))
+                {
+                    Console.WriteLine(options.Path + " isn't a directory, ending process...");
+                    return;
+                }
+                
+                Console.WriteLine("Made it past :3");
+            });
+        }
+
+        /*static string SequentialDiskUsage(string path)
+        {
+            //
+        }
+
+        static string ParallelDiskUsage(string path)
+        {
+            //
+        }*/
+    }
+}
