e9615b6eeac8c9a9c5a95f2f6c83b21c7f50bb93
Sapphire Godard <cng8060@g.rit.edu>
2024-02-19 21:47:07 -0500

    - Added parallel and single threaded functions for disk usage (No both yet) - It now prints out statistics for each function (No both yet)

--- Project_One/Program.cs
+++ Project_One/Program.cs
@@ -1,6 +1,7 @@
-﻿using CommandLine;
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using CommandLine;
 using CommandLine.Text;
-using System.IO;
 
 namespace Project_One
 {
@@ -9,10 +10,12 @@ namespace Project_One
         [Option(shortName: 's', Group = "threadMode", HelpText = "Run in single threaded mode")]
         public bool SingleThread { get; set; }
         
-        [Option(shortName: 'd', Group = "threadMode", HelpText = "Run in parallel mode (uses all available processors)")]
+        [Option(shortName: 'd', Group = "threadMode",
+            HelpText = "Run in parallel mode (uses all available processors)")]
         public bool Parallel { get; set; }
         
-        [Option(shortName: 'b', Group = "threadMode", HelpText = "Run in both parallel and single threaded mode. Runs parallel followed by sequential mode")]
+        [Option(shortName: 'b', Group = "threadMode",
+            HelpText = "Run in both parallel and single threaded mode. Runs parallel followed by sequential mode")]
         public bool Both { get; set; }
         
         [Value(index: 0, MetaName = "Path", Required = true, HelpText = "Starting directory to parse through")]
@@ -34,7 +37,11 @@ namespace Project_One
 
     public class Program
     {
-        static void Main(string[] args)
+        private static ImmutableList<string> _imgExts = ImmutableList.Create(
+            ".jpeg", ".jpg", ".gif", ".png", ".bmp", ".tiff", ".svg", ".raw", ".webp", ".avif", ".heif"
+            );
+        
+        private static void Main(string[] args)
         {
             Parser.Default.ParseArguments<ProgramOptions>(args).WithParsed(options =>
             {
@@ -43,19 +50,124 @@ namespace Project_One
                     Console.WriteLine(options.Path + " isn't a directory, ending process...");
                     return;
                 }
-                
-                Console.WriteLine("Made it past :3");
+
+                if (options.SingleThread)
+                {
+                    DateTime startTime = DateTime.Now;
+                    
+                    var (countedFolders, countedFiles, totalSpaceAmount, countedImages,
+                        imageSpaceAmount) = SequentialDiskUsage(options.Path);
+                    
+                    DateTime endTime = DateTime.Now;
+                    Double elapsedSeconds = (endTime - startTime).TotalSeconds;
+                    
+                    Console.WriteLine(
+                        "Sequential Calculated in: {0}s\n{1} folders, {2} files, {3} bytes\n{4} image files, {5} bytes",
+                        elapsedSeconds, countedFolders.ToString("N0"), countedFiles.ToString("N0"),
+                        totalSpaceAmount.ToString("N0"), countedImages.ToString("N0"),
+                        imageSpaceAmount.ToString("N0"));
+                }
+                else if (options.Parallel)
+                {
+                    DateTime startTime = DateTime.Now;
+
+                    var (countedFolders, countedFiles, totalSpaceAmount, countedImages,
+                            imageSpaceAmount) = ParallelDiskUsage(options.Path);
+                    
+                    DateTime endTime = DateTime.Now;
+                    Double elapsedSeconds = (endTime - startTime).TotalSeconds;
+                    
+                    Console.WriteLine(
+                        "Parallel Calculated in: {0}s\n{1} folders, {2} files, {3} bytes\n{4} image files, {5} bytes",
+                        elapsedSeconds, countedFolders.ToString("N0"), countedFiles.ToString("N0"),
+                        totalSpaceAmount.ToString("N0"), countedImages.ToString("N0"),
+                        imageSpaceAmount.ToString("N0"));
+                }
+                else
+                {
+                    Console.WriteLine("Not made yet :3");
+                }
             });
         }
 
-        /*static string SequentialDiskUsage(string path)
+        private static (long, long, long, long, long) SequentialDiskUsage(string path)
         {
-            //
+            string[] files = Directory.GetFiles(path);
+            string[] dirs = Directory.GetDirectories(path);
+
+            long countedFolders = 0;
+            long countedFiles = 0;
+            long totalSpaceAmount = 0;
+            long countedImages = 0;
+            long imageSpaceAmount = 0;
+
+            foreach (string file in files)
+            {
+                FileInfo currFile = new FileInfo(file);
+                
+                countedFiles++;
+                totalSpaceAmount += currFile.Length;
+
+                if (_imgExts.Contains(currFile.Extension))
+                {
+                    countedImages++;
+                    imageSpaceAmount += currFile.Length;
+                }
+            }
+
+            foreach (string dir in dirs)
+            {
+                countedFolders++;
+
+                var (cFo, cFi, tsa, ci, isa) = SequentialDiskUsage(dir);
+                countedFiles += cFo;
+                countedFiles += cFi;
+                totalSpaceAmount += tsa;
+                countedImages += ci;
+                imageSpaceAmount += isa;
+            }
+            
+            return (countedFolders, countedFiles, totalSpaceAmount, countedImages, imageSpaceAmount);
         }
 
-        static string ParallelDiskUsage(string path)
+        private static (long, long, long, long, long) ParallelDiskUsage(string path)
         {
-            //
-        }*/
+            string[] files = Directory.GetFiles(path);
+            string[] dirs = Directory.GetDirectories(path);
+
+            long countedFolders = 0;
+            long countedFiles = 0;
+            long totalSpaceAmount = 0;
+            long countedImages = 0;
+            long imageSpaceAmount = 0;
+
+            Parallel.ForEach(files, file =>
+            {
+                FileInfo currFile = new FileInfo(file);
+                
+                Interlocked.Increment(ref countedFiles);
+                Interlocked.Add(ref totalSpaceAmount, currFile.Length);
+
+                if (_imgExts.Contains(currFile.Extension))
+                {
+                    Interlocked.Increment(ref countedImages);
+                    Interlocked.Add(ref imageSpaceAmount, currFile.Length);
+                }
+            });
+            
+            Parallel.ForEach(dirs, dir =>
+            {
+                Interlocked.Increment(ref countedFolders);
+                
+                var (cFo, cFi, tsa, ci, isa) = ParallelDiskUsage(dir);
+                Interlocked.Add(ref countedFiles, cFo);
+                Interlocked.Add(ref countedFiles, cFi);
+                Interlocked.Add(ref totalSpaceAmount, tsa);
+                Interlocked.Add(ref countedImages, ci);
+                Interlocked.Add(ref imageSpaceAmount, isa);
+            });
+            
+            return (countedFolders, countedFiles, totalSpaceAmount, countedImages, imageSpaceAmount);
+        }
     }
 }
